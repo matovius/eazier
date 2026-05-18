f8e193392912b8b1bb31bc3912ad09b188e1c1b4
Martin Matovu <martin@matovius.dev>
2026-05-18 14:09:04 +0300

    feat: add `GraphInput` component
    - NOTE: it is unused as of right now, but I hope to change that in the
      future

---
+++ src/components/Graph/GraphInput.svelte
@@ -0,0 +1,15 @@
+<script>
+	/**
+	 * @typedef Props
+	 * @prop { string } id
+	 * @prop { number } [min]
+	 * @prop { number } [max]
+	 * @prop { number } [step]
+	 * @prop { number } [value]
+	 */
+
+	/** @type { Props } */
+	let { id, min = 0, max = 1, step = 0.01, value = $bindable(0) } = $props();
+</script>
+
+<input type="number" {id} class="graph-input" {min} {max} {step} bind:value />
